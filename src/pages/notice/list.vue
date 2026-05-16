2a735aca9eda4e5f45341f18007399bd06be62d4
@shayla467 <3551704957@qq.com>
2026-05-16 20:23:20 +0800

    第三次

--- src/pages/notice/list.vue
+++ src/pages/notice/list.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card shadow="never" class="border-0">
-    <!-- 新增|刷新 -->
+    <!-- 新增|刷新2 -->
     <ListHeader @create="handleCreate" @refresh="getData"/>
 
     <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
